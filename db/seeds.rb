b09d269c0f860820dbab629abfa5ac12a496cb68
JasonAkiraJordan <Jasonajordan0@gmail.com>
2021-05-01 15:27:03 -0400

    seeded test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Image.destroy_all
+
+imgSeed = "https://res.cloudinary.com/jasonjordan/image/upload/v1613589596/ajxzmzfqsamz7a2jj3fm.png"
+
+jason = User.create(username: "JasonJordan", password:"1234")
+
+img1 = Image.create(imgUrl: imgSeed, description: "This is a seeded image", user_id: jason.id)
+
